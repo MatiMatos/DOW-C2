71180888f7ee061d41a56f386fc3ad764b629fb7
MatiMatos <127316169+MatiMatos@users.noreply.github.com>
2023-06-08 15:40:39 -0400

    Cambio de paguina de inicio

--- Certamen/app/Http/Controllers/AlumnoController.php
+++ Certamen/app/Http/Controllers/AlumnoController.php
@@ -4,21 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 class AlumnoController extends Controller
-=======
-class Alumno extends Controller
->>>>>>> develop
+// =======
+// class Alumno extends Controller
+// >>>>>>> develop
 {
     public function index(){
         //dd('Hola mundo'); //dump and die
         return view('alumno.alumno');
     }
-<<<<<<< HEAD
+/* <<<<<<< HEAD
 
-    /* public function inicio(){
+     public function inicio(){
         return view('bienvenida.index');
-    } */
-=======
->>>>>>> develop
+    } 
+    =======
+    >>>>>>> develop */
 }
